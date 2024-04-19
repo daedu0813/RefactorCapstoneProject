0d15800e7e9b4764bc1f257966790630b79277fd
DAEDUKIM <daedu0813@gmail.com>
2024-04-19 14:03:06 +0900

    init

---
+++ src/main/java/com/daedukim/RefactorCapstoneProject/RefactorCapstoneProjectApplication.java
@@ -0,0 +1,13 @@
+package com.daedukim.RefactorCapstoneProject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RefactorCapstoneProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RefactorCapstoneProjectApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/daedukim/RefactorCapstoneProject/RefactorCapstoneProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.daedukim.RefactorCapstoneProject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RefactorCapstoneProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
